0bdcea5b306306281ea77401524a7d7221e9705e
Andrew vonderLuft <avonderluft@avlux.net>
2011-04-22 15:46:19 -0700

    eliminate duplicates.

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -1,12 +1,12 @@
 class Admin::SearchController < ApplicationController
   def index
     if params[:q]
-      @pages = Page.search(params[:q])
+      @pages = Page.search(params[:q]).uniq
       if current_user.designer? || current_user.admin?
-        @snippets = Snippet.search(params[:q])
-        @layouts = Layout.search(params[:q])
-        @templates = Template.search(params[:q]) if defined?(TemplatesExtension)
-        @banners = Banner.search(params[:q]) if defined?(BannerRotatorExtension)
+        @snippets = Snippet.search(params[:q]).uniq
+        @layouts = Layout.search(params[:q]).uniq
+        @templates = Template.search(params[:q]).uniq if defined?(TemplatesExtension)
+        @banners = Banner.search(params[:q]).uniq if defined?(BannerRotatorExtension)
       end
     end
   end
